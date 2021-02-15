d85da6761270782df528e161b12aade5ad30eb06
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-15 22:13:32 +0300

    Ref

--- GoodDay/GoodDay/Greeting.cs
+++ GoodDay/GoodDay/Greeting.cs
@@ -4,6 +4,25 @@ namespace GoodDay
 {
     class Greeting
     {
+        public static void Meth()
+        {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            if (DateTime.Now.Hour > 8 & DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (DateTime.Now.Hour > 11 & DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("Good day, guys");
+            }
+            else if (DateTime.Now.Hour > 14 & DateTime.Now.Hour < 23)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+            else
+            {
 
+            }
+        }
     }
 }
--- GoodDay/GoodDay/Program.cs
+++ GoodDay/GoodDay/Program.cs
@@ -7,23 +7,8 @@ namespace GoodDay
         static void Main(string[] args)
         {
             {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
-                if (DateTime.Now.Hour > 8 & DateTime.Now.Hour < 12)
-                {
-                    Console.WriteLine("Good morning, guys");
-                }
-                else if (DateTime.Now.Hour > 11 & DateTime.Now.Hour < 15)
-                {
-                    Console.WriteLine("Good day, guys");
-                }
-                else if (DateTime.Now.Hour > 14 & DateTime.Now.Hour < 23)
-                {
-                    Console.WriteLine("Good evening, guys");
-                }
-                else
-                {
-
-                }
+                
+                Greeting.Meth();
             }
         }
     }
