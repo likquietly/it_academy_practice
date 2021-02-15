da6734502119f5f461dd8764e413b24db94e9ab9
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-15 22:07:50 +0300

    Add Project GoodDay

--- GoodDay/GoodDay/Program.cs
+++ GoodDay/GoodDay/Program.cs
@@ -6,7 +6,23 @@ namespace GoodDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            {
+                if (DateTime.Now.Hour > 8 & DateTime.Now.Hour < 12)
+                {
+                    Console.WriteLine("Good morning, guys");
+                }
+                else if (DateTime.Now.Hour > 11 & DateTime.Now.Hour < 15)
+                {
+                    Console.WriteLine("Good day, guys");
+                }
+                else if (DateTime.Now.Hour > 14 & DateTime.Now.Hour < 23)
+                {
+                    Console.WriteLine("Good evening, guys");
+                }
+                else
+                {
+
+                }
+            }
     }
 }
