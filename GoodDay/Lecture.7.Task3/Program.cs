ced6e35cd21c7f787284ff1d884a1b3ec9de4db3
likquietly <45600764+likquietly@users.noreply.github.com>
2021-02-22 20:48:12 +0300

    Lecture.7.Task3 Added

---
+++ GoodDay/Lecture.7.Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lecture._7.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userArr = new int[13];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < userArr.Length; i++)
+            {
+                userArr[i] = rnd.Next(13);
+                Console.Write(userArr[i] + " ");
+            }
+
+            Array.Sort(userArr);
+            int maxValue = userArr[userArr.Length - 1];
+            Console.WriteLine("Max value is: " + maxValue);
+        }
+    }
+}
