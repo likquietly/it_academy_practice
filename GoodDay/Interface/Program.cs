d305bf09af117158ff59aa4849f15dbd61bd949f
likquietly <45600764+likquietly@users.noreply.github.com>
2021-03-17 19:52:43 +0300

    ref

--- GoodDay/Interface/Program.cs
+++ GoodDay/Interface/Program.cs
@@ -21,6 +21,13 @@ namespace Interface
 
             Triangle triangle2 = new(6, 9);
             triangle2.Area();
+
+            object[] mass = new object[5];
+            mass[0] = circle;
+            mass[1] = circle2;
+            mass[2] = square;
+            mass[3] = triangle;
+            mass[4] = triangle2;
         }
     }
 }
