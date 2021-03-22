5111b2f7f035bee714987ee76371198b55e31d75
likquietly <45600764+likquietly@users.noreply.github.com>
2021-03-22 19:27:38 +0300

    Added Project Uniqueitem

---
+++ GoodDay/Uniqueitem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uniqueitem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uniqueitem item1 = new(1000);
+            Console.WriteLine(Uniqueitem.id);
+
+            Uniqueitem item2 = new();
+            Console.WriteLine(Uniqueitem.id);
+
+            Uniqueitem item3 = new();
+            Console.WriteLine(Uniqueitem.id);
+        }
+    }
+}
---
+++ GoodDay/Uniqueitem/Uniqueitem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniqueitem
+{
+    class Uniqueitem
+    {
+        public static int id = 0;
+
+        public Uniqueitem()
+        {
+            id++;
+        }
+
+        public Uniqueitem(int newId)
+        {
+            id = newId;
+        }
+    }
+}
