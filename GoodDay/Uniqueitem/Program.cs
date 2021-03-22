07ba10369ae4d02dc79c31bf8ab4c9ec6283b7a6
likquietly <45600764+likquietly@users.noreply.github.com>
2021-03-22 19:33:52 +0300

    Refactoring comment

--- GoodDay/Uniqueitem/Program.cs
+++ GoodDay/Uniqueitem/Program.cs
@@ -7,20 +7,20 @@ namespace Uniqueitem
         static void Main(string[] args)
         {
             Uniqueitem item1 = new();
-            Console.WriteLine(Uniqueitem.id);
+            Console.WriteLine(Uniqueitem.id);  //1
 
             Uniqueitem item2 = new();
-            Console.WriteLine(Uniqueitem.id);
+            Console.WriteLine(Uniqueitem.id);  //2
 
             Uniqueitem item3 = new(1000);
-            Console.WriteLine(Uniqueitem.id);
+            Console.WriteLine(Uniqueitem.id);  //1001
 
             Uniqueitem item4 = new();
-            Console.WriteLine(Uniqueitem.id);
+            Console.WriteLine(Uniqueitem.id);  //1002
 
             Uniqueitem.id = 2000;
             Uniqueitem item5 = new();
-            Console.WriteLine(Uniqueitem.id);
+            Console.WriteLine(Uniqueitem.id);  //2001
         }
     }
 }
