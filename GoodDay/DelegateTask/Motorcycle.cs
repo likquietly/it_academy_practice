bc2cddd0d8cbc4f14e468a82d457d78238e64f14
likquietly <45600764+likquietly@users.noreply.github.com>
2021-04-12 19:58:19 +0300

    Commit

---
+++ GoodDay/DelegateTask/Motorcycle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateTask
+{
+    class Motorcycle
+    {
+        public Guid Id { get; private set; }
+        public string Model { get; set; }
+        public int Odometer { get; set; }
+        public string MadeIn { get; set; }
+    }
+}
---
+++ GoodDay/DelegateTask/MotorcycleArray.cs
@@ -0,0 +1,16 @@
+﻿namespace DelegateTask
+{
+    class MotorcycleArray
+    {
+        static Motorcycle[] arr = new Motorcycle[5];
+
+        public MotorcycleArray()
+        {
+            arr[0] = new Motorcycle { Model = "Minsk X250", MadeIn = "China", Odometer = 6_000 };
+            arr[1] = new Motorcycle { Model = "Honda CB500F", MadeIn = "Japan", Odometer = 11_500 };
+            arr[2] = new Motorcycle { Model = "BMW G1200R", MadeIn = "Germany", Odometer = 20_000 };
+            arr[3] = new Motorcycle { Model = "Triumph Tiger 800", MadeIn = "China", Odometer = 30_000 };
+            arr[4] = new Motorcycle { Model = "Honda CB1100RR", MadeIn = "Japan", Odometer = 26_000 };
+        }
+    }
+}
